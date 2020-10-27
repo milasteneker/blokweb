2df46b35f3e88c2c28bb19b94c12d19a8907abc1
milasteneker <70643876+milasteneker@users.noreply.github.com>
2020-10-27 12:49:21 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+var hamburgerMenu = document.querySelector ('#hamburger');
+var	hamburgerButton = document.querySelector ('#hamburgerButton')
+var kruisjeButton = document.querySelector ('#kruisje');
+
+hamburgerMenu.hidden=true;
+
+function toonMenu () {
+	hamburgerMenu.style.visibility='visible';
+}
+
+function verstopMenu () {
+	hamburgerMenu.style.visibility='hidden';
+}
+
+hamburgerButton.addEventListener('click', toonMenu);
+kruisjeButton.addEventListener('click', verstopMenu);
+
+
